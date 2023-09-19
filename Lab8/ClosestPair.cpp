45cb9d2cffbfc3ecb6979288270affb98e3cd83b
Sania Khokhar <saniaj963@gmail.com>
2023-09-19 16:22:34 +0530

    aa_lab

--- Lab8/ClosestPair.cpp
+++ Lab8/ClosestPair.cpp
@@ -18,30 +18,41 @@ void minDistance(Point P[]){
     for( i = 0; i < 3; i++){
          for( j = i+1; j < 3; j++){
             d = sqrt(pow(P[i].x - P[j].x, 2) + pow(P[i].y - P[j].y,2)); 
+            if(dmin > d){
+                dmin = d;
+                indi = i;
+                indj = j;
+            } 
          }       
-         if(dmin > d){
-            dmin = d;
-            indi = i;
-            indj = j;
-         } 
+         
     }
     cout << "Minimum distance is  between point P" << indi  << " and P"  << indj << endl;
-    cout <<  "Minimum distance is " << dmin ;
+    cout <<  "Minimum distance is " << dmin << endl;
 }
 
-int main(){
-    Point P[3];
-    P[0].x =2;
-    P[0].y =2;
-    P[1].x =4;
-    P[1].y =4;
-    P[2].x =6;
-    P[2].y =6;
+int main(){int n;
+    cout << "Enter number of points" << endl;
+    cin >> n ;
+
+    cout << "Enter points " << endl;
+    Point P[n];
+    for(int i = 0; i < n; i++){
+        cin >> P[i].x >> P[i].y;
+    }
+
     minDistance(P);
 
     return 0;
 }
 
+//Input:
+//Enter number of points
+// 3
+// Enter points
+// 2 4
+// 1 2
+// 5 9
+
 //Output:
-//Minimum distance is  between point P1 and P3
-// Minimum distance is 2.82843
+// Minimum distance is  between point P0 and P1
+// Minimum distance is 2.23607
